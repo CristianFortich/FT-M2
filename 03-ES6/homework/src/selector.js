56a5f5d0cca6584e4d79546dfd2f1f2bc20b8c28
Cristian Fortich <yezidfortich@gmail.com>
2022-05-02 22:43:28 -0500

    solves homework 03

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,22 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  let sons = startEl.children;
+  if(matchFunc(startEl)){
+    resultSet.push(startEl);
+  }
+  if(sons.length>0){
+    for (let i = 0; i < sons.length; i++) {
+      let son = sons[i];
+      let arr = traverseDomAndCollectElements(matchFunc, son);
+      resultSet = [...resultSet, ...arr];
+    }
+    /* sons.forEach(element => {
+      let arr = traverseDomAndCollectElements(matchFunc, element);
+      resultSet = [...resultSet, ...arr];
+    }); */
+  }
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +33,17 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  let st;
+  if(selector[0]=='#'){
+    st='id';
+  }else if(selector[0]=='.'){
+    st='class';
+  }else if(selector.includes('.')){
+    st='tag.class';
+  }else{
+    st='tag';
+  }
+  return st;
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +55,32 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction = function(element){
+      return element.id == selector.slice(1);
+    }
   } else if (selectorType === "class") {
-    
+    matchFunction = function(element){
+      let str = element.classList;
+      let value = false;
+      str.forEach(el => {
+        if(el==selector.slice(1)) value = true;
+      });
+      return value;
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function(element){
+      let tag = selector.split('.');
+      let str = element.classList;
+      let value = false;
+      str.forEach(el => {
+        if(el==tag[1]) value = true;
+      });
+      return value && (tag[0]==element.tagName.toLowerCase());
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = function(element){
+      return selector == element.tagName.toLowerCase();
+    }
   }
   return matchFunction;
 };
